a8f00ee0fd4251303d466a3c0b6f246e82569b59
Hasnain-iut <hasnainkabir@iut-dhaka.edu>
2023-08-14 02:14:49 +0600

    webapp

---
+++ Projects/Wallet Webapp/app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SubmitField
+from wtforms.validators import InputRequired
+from derivbot import Account
+import asyncio
+import threading
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "nibirkabir"
+
+class LoginForm(FlaskForm):
+    api_key = StringField(validators=[InputRequired()], 
+                          render_kw={"placeholder" : "Enter API Token"})
+    app_id = IntegerField(validators=[InputRequired()],
+                          render_kw={"placeholder": "Enter APP ID"})
+
+    submit = SubmitField("Save Information", render_kw={"class": "save-btn"})
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        account = Account(api_token=form.api_key.data, app_id=form.app_id.data)
+        thread = threading.Thread(target=asyncio.run, args=(account.do_authorization(),))
+        thread.start()
+        thread.join()
+
+        return render_template('login.html', form=form, hide_loading_overlay=True, show_true_modal=True)
+
+    return render_template('login.html', form=form)
+
+@app.route('/info')
+
+def info():
+    return render_template('info.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ Projects/Wallet Webapp/derivbot.py
@@ -0,0 +1,25 @@
+
+from deriv_api import DerivAPI
+
+
+class Account:
+    def __init__(self, api_token, app_id):
+        self.api_token = api_token
+        self.app_id = app_id
+        self.status = False
+
+    def change_status(self):
+        self.status = True
+    
+    async def do_authorization(self):
+        try:
+            global api 
+            api = DerivAPI(app_id = self.app_id)
+            authorize = await api.authorize(self.api_token)
+
+            print("Authorization Successful")
+            print(authorize)
+            self.status=True
+        except Exception as e:
+            print(f'Authorization error occurred: {e}')
+        
